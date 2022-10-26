6784ea65d92c956298c3922e576c93a50f647dfc
Kristina Peregudova <k.peregudova@redmadrobot.com>
2022-10-26 16:05:20 +0700

    Initial Commit

---
+++ SwiftUI+Alamofire/SwiftUI_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AlamofireApp.swift
+//  SwiftUI+Alamofire
+//
+//  Created by Кристина Перегудова on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
